079947263f9b0332c99c9efdb99b753902bd69f5
rezwanrahimr <rezwanrahim69@gmail.com>
2022-08-22 23:31:17 +0600

    banner added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './pages/Shear/Navbar';
 import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
       <Routes>
-        <Route></Route>
+        <Route path='/' element={<Home></Home>}>Home</Route>
       </Routes>
     </div>
   );
---
+++ src/pages/Home/Banner.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import chair from '../../assets/images/chair.png'
+import BannerStyle from './BannerStyle.css';
+
+const Banner = () => {
+    return (
+        <div class="hero min-h-screen p-6">
+            <div class="hero-content flex-col lg:flex-row-reverse p-7 bannerImg">
+                <img src={chair} class="max-w-sm rounded-lg shadow-2xl" />
+                <div className='bannerText '>
+                    <h1 class="text-5xl font-bold text-left">Your New Smile Starts <br></br> Here</h1>
+                    <p class="py-6 text-left">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the</p>
+                    <button class="btn btn-primary flex justify-start">Get Started</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
