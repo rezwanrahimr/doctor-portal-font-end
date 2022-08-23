8e45d7402452c4ef75021227749575862b6a5e61
rezwanrahimr <rezwanrahim69@gmail.com>
2022-08-23 23:25:35 +0600

    next section complite

--- src/pages/Home/Location.js
+++ src/pages/Home/Location.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BsFillClockFill } from "react-icons/bs";
 import clock from "../../assets/images/clock.svg"
+import location from "../../assets/images/marker.svg"
+import phone from "../../assets/images/phone.svg"
 import LocationCss from './Location.css'
 
 
@@ -11,20 +13,32 @@ const Location = () => {
         <div className='flex justify-between  mx-6 flex-col md:flex-row '>
 
             <div className=' item1 my-2 flex items-center'>
-                       
-                            <div class="grid justify-start flex items-center"><img className='mx-3'width={50} src={clock} alt="" /></div>
 
-                            <div class="grid text-left flex items-center text-center clockText">
-                                <h1 className='text-left'>Opening Hours</h1>
-                                <p>Lorem ipsum dolor sit amet.</p>
-                            </div>
-                       
-                   
-                   
-                
+                <div class="grid justify-start flex items-center"><img className='mx-3' width={50} src={clock} alt="" /></div>
+
+                <div class="grid text-left flex items-center text-center clockText">
+                    <h1 className='text-left'>Opening Hours</h1>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                </div>
+
+
             </div>
-            <div className=' item2 my-2'>{BsFillClockFill}</div>
-            <div className=' item3 my-2'></div>
+            <div className=' item2 my-2 flex items-center'>
+                <div class="grid justify-start flex items-center"><img className='mx-3' width={50} src={location} alt="" /></div>
+
+                <div class="grid text-left flex items-center text-center clockText">
+                    <h1 className='text-left'>Visit our location</h1>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                </div>
+            </div>
+            <div className=' item3 my-2 flex items-center'>
+                <div class="grid justify-start flex items-center"><img className='mx-3' width={50} src={phone} alt="" /></div>
+
+                <div class="grid text-left flex items-center text-center clockText">
+                    <h1 className='text-left'>Contact us now</h1>
+                    <p>+000 123 456789</p>
+                </div>
+            </div>
         </div>
     );
 };
